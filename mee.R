df7a0d42f666c122303330b7e0d1e4dbe6e2fbc7
Danilo Monteiro <danilomonteiroo@gmail.com>
2020-10-30 13:52:45 -0300

    Update mee.R

--- mee.R
+++ mee.R
@@ -2,77 +2,21 @@ library(sem)
 library(lavaan)
 library(semTools)
 library(sqldf)
-dados<-read.csv2(file = "teste/doutoradoModelo20.csv",header = TRUE, sep = ";", dec=",") # abrir arquivo
+dados<-read.csv2(file = "GitHub/doutoradoR/doutoradoModelo20.csv",header = TRUE, sep = ";", dec=",") # abrir arquivo
 
 names(dados)
 
 modelo.v1 <- '
 
-#STRESS =~ S1+S2+S3
-#EMOCAO =~ EM1+ EM2 +EM3+ EM4
-#INTER =~ I1+I2+I3+I4
-#CRIATIVIDADE =~ C2+C3+C4
+STRESS =~ S1+S2+S3
+EMOCAO =~ EM1+ EM2 +EM3+ EM4
+INTER =~ I1+I2+I3+I4
+CRIATIVIDADE =~ C2+C3+C4
 TREIN =~ T1+T2+T3+T4
 
 T1~~T2
 
-#EM2 ~~ EM3
-
-
-SATISFACAO=~ sat1+sat2+sat4
-
-
-#SATISFACAO ~ STRESS
-#0.365
-#SATISFACAO ~ EMOCAO
-#0.354
-#SATISFACAO ~ INTER
-# 0.354
-#SATISFACAO ~ CRIATIVIDADE
-# 0.286
-#SATISFACAO ~TREIN
-# 0.247
-
-
-instabilidadeTarefas =~ reque2+reque3+req4+req5+req6+req7
-instabilidadeEquipe=~ equipe1+equipe2+equipe3
-
-
-#SATISFACAO ~instabilidadeTarefas
-# - 0.201
-#SATISFACAO ~instabilidadeEquipe
-# - 0.287
-
-
-#SATISFACAO ~ a*STRESS + b*instabilidadeTarefas
-# -0.064 p 0.005
-#SATISFACAO ~ a*STRESS + b*instabilidadeEquipe
-# -0.101 p 0.001
-
-#SATISFACAO ~ a*EMOCAO + b*instabilidadeTarefas
-# -0.065 p 0.003
-#SATISFACAO ~ a*EMOCAO + b*instabilidadeEquipe
-# -0.110
-
-#SATISFACAO ~ a*INTER + b*instabilidadeTarefas
-# -0.052 p 0.008
-#SATISFACAO ~ a*INTER + b*instabilidadeEquipe
-# -0.080 0.002
-
-#SATISFACAO ~ a*CRIATIVIDADE + b*instabilidadeTarefas
-# -0.032 p 0.046
-#SATISFACAO ~ a*CRIATIVIDADE + b*instabilidadeEquipe
-# -0.038 0.064
-
-
-#SATISFACAO ~ a*TREIN + b*instabilidadeTarefas
-# -0.036 p 0.014
-#SATISFACAO ~ a*TREIN + b*instabilidadeEquipe
-# -0.063 0.005
-
-
-
-ei:= a*b
+EM2 ~~ EM3
 
 ' #verificar modelo adaptabilidade
 
