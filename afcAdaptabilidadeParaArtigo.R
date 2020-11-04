493aa709da63bbf8c2f65788f592c7203e5bc715
Danilo Monteiro <danilomonteiroo@gmail.com>
2020-11-04 00:23:49 -0300

    novo modelo completo e afc burnout
    Novos testes realizados

--- afcAdaptabilidadeParaArtigo.R
+++ afcAdaptabilidadeParaArtigo.R
@@ -2,15 +2,13 @@ library(sem)
 library(lavaan)
 library(semTools)
 library(sqldf)
-library(readxl)
-install.packages("installr")
-library(installr)
-updateR()
-dadosAdaptabilidade <- read_excel("")
+library(semMediation)
+library(processR) #install.packages("processR")
+#dados<-read.csv2(file = "GitHub/doutoradoR/doutoradoModelo20.csv",header = TRUE, sep = ";", dec=",") # abrir arquivo
+dados<-read.csv2(file = "doutoradoModelo20.csv",header = TRUE, sep = ";", dec=",") # abrir arquivo
 
-dadosAdp<-read.csv2(file = "dadosAdaptabilidade.csv",header = TRUE, sep = ";", dec=",") # abrir arquivo
 
-names(dadosAdp)
+names(dados)
 
 modelo.v1 <- '
 
@@ -18,18 +16,36 @@ STRESS =~ S1+S2+S3
 EMOCAO =~ EM1+ EM2 +EM3+ EM4
 INTER =~ I1+I2+I3+I4
 CRIATIVIDADE =~ C1+C2+C3+C4
-TREIN =~ T1+T2+T3+T4
-
-T1~~T2
-EM2 ~~ EM3
-S3 ~~  C1
+TREIN =~ T1+T2+T3+
 
 ' #verificar modelo adaptabilidade
 
-modelo.v1.fit <- cfa(modelo.v1, data=dadosAdp, std.lv=TRUE,ordered=TRUE ) 
+modelo.v2 <- '
+
+STRESS =~ S1+S2+S3
+EMOCAO =~ EM1+ EM2 +EM3+ EM4
+INTER =~ I1+I2+I3+I4
+CRIATIVIDADE =~ C1+C2+C3+C4
+TREIN =~ T1+T2+T3+T4
+
+
+STRESS=~1*EMOCAO
+
+
+' #verificar modelo adaptabilidade
+
+modelo.v1.fit <- cfa(modelo.v1, data=dados, std.lv=TRUE,ordered = TRUE  ) 
+modelo.v2.fit <- cfa(modelo.v2, data=dados, std.lv=TRUE, ordered = TRUE ) 
+
+
+anova(modelo.v1.fit, modelo.v2.fit)
+
+reliability(modelo.v1.fit)
 summary(modelo.v1.fit, fit.measures = TRUE, rsquare =TRUE, standardized = TRUE) # fitMeasures(modelo.v1.fit)
+summary(modelo.v2.fit, fit.measures = TRUE, rsquare =TRUE, standardized = TRUE)
 lavInspect(modelo.v1.fit,"cor.lv")
 fitMeasures(modelo.v1.fit)
+fitMeasures(modelo.v2.fit)
 modificationindices(modelo.v1.fit, sort =TRUE, minimum.value = 9)
 reliability(modelo.v1.fit)
 discriminantValidityTable(modelo.v1.fit)
--- afcInstabilidade.R
+++ afcInstabilidade.R
@@ -2,8 +2,8 @@ library(sem)
 library(lavaan)
 library(semTools)
 library(sqldf)
-dados<-read.csv2(file = "GitHub/doutoradoR/doutoradoModelo20.csv",header = TRUE, sep = ";", dec=",") # abrir arquivo
-
+#dados<-read.csv2(file = "GitHub/doutoradoR/doutoradoModelo20.csv",header = TRUE, sep = ";", dec=",") # abrir arquivo
+dados<-read.csv2(file = "doutoradoModelo20.csv",header = TRUE, sep = ";", dec=",") # abrir arquivo
 names(dados)
 
 modelo.v1 <- '
@@ -15,8 +15,25 @@ instabildiadeTecnica=~ tec1+tec2
 
 ' #verificar modelo adaptabilidade
 
-modelo.v1.fit <- cfa(modelo.v1, data=dados, std.lv=TRUE,ordered=TRUE ) 
+modelo.v2 <- '
+
+instabilidadeTarefas =~ reque2+reque3+req5+req6+req7
+instabilidadeEquipe=~ equipe1+equipe2+equipe3
+instabildiadeTecnica=~ tec1+tec2
+
+instabilidadeTarefas=~ 1*instabilidadeEquipe
+
+
+
+
+' #verificar modelo adaptabilidade
+
+modelo.v1.fit <- cfa(modelo.v1, data=dados, std.lv=TRUE,ordered = TRUE  ) 
+modelo.v2.fit <- cfa(modelo.v2, data=dados, std.lv=TRUE, ordered = TRUE ) 
+anova(modelo.v1.fit, modelo.v2.fit)
+
 summary(modelo.v1.fit, fit.measures = TRUE, rsquare =TRUE, standardized = TRUE) # fitMeasures(modelo.v1.fit)
+summary(modelo.v2.fit, fit.measures = TRUE, rsquare =TRUE, standardized = TRUE) # fitMeasures(modelo.v1.fit)
 lavInspect(modelo.v1.fit,"cor.lv")
 fitMeasures(modelo.v1.fit)
 modificationindices(modelo.v1.fit, sort =TRUE, minimum.value = 9)
